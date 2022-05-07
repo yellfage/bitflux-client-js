21c691de4f672414e033193d932050ecd0b9af0f
iRandell <irandellhart@gmail.com>
2022-05-07 20:00:25 +0300

    refactor: restructure 'reconnection' namespace

---
+++ package/bitflux-client/src/reconnection/control/index.ts
@@ -0,0 +1,6 @@
+export * from './reconnection-control'
+export * from './reconnection-control-builder'
+export * from './basic-reconnection-control'
+export * from './basic-reconnection-control-builder'
+export * from './disabled-reconnection-control'
+export * from './disabled-reconnection-control-builder'
---
+++ package/bitflux-client/src/reconnection/delay/index.ts
@@ -0,0 +1,4 @@
+export * from './reconnection-delay-scheme'
+export * from './reconnection-delay-scheme-builder'
+export * from './basic-reconnection-delay-scheme'
+export * from './basic-reconnection-delay-scheme-builder'
